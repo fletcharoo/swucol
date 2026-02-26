a463b5dd1d6e9e149aca24b961b03677a8d8003f
FletcherCutting <fcutting@hotmail.com>
2026-02-26 23:16:21 +1100

    Start REST API

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// helloHandler responds with "hello world" for GET /hello requests.
+func helloHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(responseWriter, "hello world")
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server listening on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
 }
+
